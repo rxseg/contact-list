8e0e665fca89ce8b60c5edb40e9584376ff8ef4b
Robert <rguerrra.training@wowdesarrollos.com>
2022-10-18 16:01:54 -0500

    mi lista de contacto v2

--- ls.js
+++ ls.js
@@ -1,74 +1,74 @@
-let acccion = "";
-let verificacion = true;
-let numero = 0 ;
-let i = 5 ;
-//let contactosAmostrar = "";
-let items = 0;
-let fin = "";
-const listaContactos = {
-    Contacto1 : "Luis Antonio",
-    Contacto2 : "Robert Guerra",
-    Contacto3 : "Jose Najera",
-    Contacto4 : "Luis Hernandez Goenaga"
+const contactList = [
+    {id: 1,
+    nombre: "Robert",
+    apellido: "Guerra",
+    telefono: 42465212554,
+    ubicacion:{
+        ciudad: "Barranquilla",
+        direccion: "49C 75-14"
+    }
+},
+    {id: 2,
+    nombre: "Fede",
+    apellido: "Valverde",
+    telefono: "No tengo ni idea",
+    ubicacion:{
+        ciudad: "Madrid",
+        direccion: "Ni idea"
+    }
 }
-mostarlista();
-do {
-    do {
-        if(!verificacion){
-            alert('Digita una opcion valida')
-        }
-        acccion = prompt("Accion,\nIngrese el numero 1 para guardar,\nIngrese el numero 2 para eliminar,\nIngrese el numero 3 para modificar", "");
-        if (isNaN(acccion)) {
-            verificacion = false;
-        }else{
-            verificacion = true ;
-            numero = parseInt(acccion);
-            if (numero > 3 || numero < 1 || acccion == '') {
-                verificacion = false;
-            }
-        }
-    } while (!verificacion);
+]
 
-    let contacto = "";
-    let newcontacto = "";
-    switch (numero) {
-        case 1:
-            contacto = prompt("Ingrese el contacto que desea agregar");
-            guardar(contacto);
-            break;
-        case 2:
-            contacto = prompt("Ingrese el contacto que desea eliminar");
-            eliminar(contacto);
-            break;
-        case 3:
-            contacto = prompt("Ingrese un número para modificar");
-            newcontacto = prompt("Ingrese modificación");
-            modificar(contacto, newcontacto);
-            break;
+const addContact = (contacto = "", id="", apellido="", telefono="", ciudad="", direccion="") =>{
+    if(!contacto) return console.warn("No has ingresado ningún contacto");
+    if(!id) return console.warn("No has ingresado un identificador");
+    if(contacto){
+        contactList.push({id : id ,nombre : contacto, apellido, telefono, ubicacion:{ciudad: ciudad, direccion: direccion}});
+        console.info(`Has agregado a ${contacto} a la lista`);
+        mostrarLista(contactList);
     }
-    fin = prompt("Desea terminar,\n Digite si/no,", "");
-} while (fin != 'si' );
-function guardar (contactoGuardar) {
-    listaContactos["Contacto"+i] = contactoGuardar;
-    i++;
-    mostarlista();
 }
-function eliminar(contactoEliminar) {
-    delete listaContactos[contactoEliminar];
-    mostarlista();
+const delContact = (contacto, id) =>{
+    if(!contacto) return console.warn("No has ingresado ningún contacto");
+    if(!id) return console.warn("No has ingresado un identificador");
+    
+        contactList.map( (element, i) => {
+            if (element.nombre === contacto) {
+                contactList.splice(i,1);
+                console.info(`Has borrado a ${contacto} de la lista`);
+                mostrarLista(contactList);
+            }
+        })
+    
 }
-function modificar(contactoModificar, newcontac) {
-    listaContactos[contactoModificar] = newcontac;
-    mostarlista();
-}
-function mostarlista() {
-/*  contactosAmostrar = "";
-    items = Object.values(listaContactos);
-    for (let i = 1; i < items.length + 1; i++) {
-        if(listaContactos["Contacto"+i] != undefined ){
-            contactosAmostrar += listaContactos["Contacto"+i]+" \n";
+const modifyList = ( contacto = "", id, apellido="", telefono = "", ciudad = "", direccion ="") =>{
+    if(!contacto) return console.warn("No has ingresado un contacto a modificar");
+    if(!id) return console.warn("No has ingresado el id del contacto a modificar");
+    contactList.forEach( (element, index) => {
+        if (element.id === id) {
+                const obj={
+                    id: id,
+                nombre: contacto,
+                apellido: apellido,
+                telefono: telefono,
+                ubicacion:{
+                    ciudad: ciudad,
+                    direccion: direccion
+                }
+            }
+            contactList.splice(index,1,obj);
+            console.info(`Has modificado a ${contacto} de la lista`);
+            mostrarLista(contactList);
         }
-    }
-    alert(contactosAmostrar); */
-    console.log(listaContactos)
+    
+    })
+
 }
+function mostrarLista (){
+    contactList.forEach((element)=>{
+        console.log(element);
+    })
+}
+addContact("Andriy", 3, "Schevchenko", 2151256512, "Kiev", "Ni idea" );
+modifyList("Andriy", 3, "Sheva", 555555555, "Malambo", "Donde el diablo dejó los hijos");
+delContact("Andriy", 3);
